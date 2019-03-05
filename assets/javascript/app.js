58ee5e56e831d8315c7c9402bff45b32aa34e90c
ANS <36783010+answebdev@users.noreply.github.com>
2019-03-04 18:56:47 -0800

    re-styled landing page

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//Landing Page Animation
+$(document).ready(function () {
+    setTimeout(function () {
+        $("#main").removeClass("is-loading");
+    }, 100)
+});
+
+// Back-to-top-button
+// var btn = $('#button');
+
+// $(window).scroll(function () {
+// 	if ($(window).scrollTop() > 300) {
+// 		btn.addClass('show');
+// 	} else {
+// 		btn.removeClass('show');
+// 	}
+// });
+
+// btn.on('click', function (e) {
+// 	e.preventDefault();
+// 	$('html, body').animate({ scrollTop: 0 }, '300');
+// });
